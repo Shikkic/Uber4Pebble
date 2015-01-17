1c5a799f44552f48147bb65f5a9bc88ad106c289
Dan Cadden <Shikkic@users.noreply.github.com>
2015-01-17 07:49:43 -0500

    I broke something

--- src/app.js
+++ src/app.js
@@ -18,76 +18,49 @@ var main = new UI.Card({
 
 main.show();
 
-/*
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-*/
-
-
-
 main.on('click', 'select', function(e) {
-  Vibe.vibrate('long');
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-30-bold',
-    text: 'An uber is on your way!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-  var butt = {"test": "ok"};
-    ajax({
-        method: 'post',
-        type: 'json',
-        url: 'http://uberme.cloudapp.net/api/test',
-        data: butt,
-        //crossDomain: true
+  
+    Vibe.vibrate('long');
+    var wind = new UI.Window();
+    var textfield = new UI.Text({
+        position: new Vector2(0, 50),
+        size: new Vector2(144, 30),
+        font: 'gothic-18-bold',
+        title: 'An uber is on your way!',
+        textAlign: 'center'
+    });
+    
+    wind.add(textfield);
+    wind.show();
+    
+    var butt = {"test": "ok"};
+        ajax({
+            method: 'post',
+            type: 'json',
+            url: 'http://uberme.cloudapp.net/api/test',
+            data: butt,
         },
-         function(success){
-           console.log("it worked!");  
-         },
-         function(error){
-           console.log("it didn't work here's why: " + error);
-         }
-      
+        function(success){
+            console.log("it worked!");  
+        },
+        function(error){
+            console.log("it didn't work here's why: " + error);
+        }
       );
     
-  wind.on('click', 'down', function(e) {
-    var card = new UI.Card();
-    card.title('Cancel');
-    card.subtitle('Are you sure?');
-    card.body('Press select to cancel ride');
-    card.show();
+    wind.on('click', 'down', function(e) {
+        var card = new UI.Card();
+        card.title('Cancel');
+        card.subtitle('Are you sure?');
+        card.body('Press select to cancel ride');
+        card.show();
       
-      card.on('click', 'select', function(e){
-          Vibe.vibrate('long');      
-          var newCard = new UI.Card();
-          newCard.title("RIDE CANCLED");
-          newCard.body('Thanks for using Uber for pebble anyway!');
-      })
-  });
-  
-  
+        card.on('click', 'select', function(e){
+            card.hide();
+            Vibe.vibrate('long');      
+            var newCard = new UI.Card();
+            newCard.title("RIDE CANCLED");
+            newCard.body('Thanks for using Uber for pebble anyway!');
+        });
+    });
 });
-
-
-
